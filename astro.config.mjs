faa096e7acd7f25a02d59e8d338875a020461931
David Viuche <jviucheroja@uniminuto.edu.co>
2022-11-26 22:19:18 -0500

    add deploy git hub actions gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,7 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: 'https://david-viuche.github.io/',
+  base: '/world_cup_2022'
 })
