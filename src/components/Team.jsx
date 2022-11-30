679d3a4cbde7d69c13d20ad0f3ce9500dfedc70a
David Viuche <jviucheroja@uniminuto.edu.co>
2022-11-29 20:44:39 -0500

    fix flags bug border and add images alt

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,7 +1,9 @@
 export const Team = ({ nombre, bandera, abrev, goles, clas }) => {
   return (
     <div className='flex justify-center items-center flex-col '>
-      <a href={`/world_cup_2022/equipo/${abrev}`}><img className={`${(!clas) ? 'h-16' : 'h-10'} w-28 pb-1 xs:w-16 xs:h-10 sm:w-28 sm:h-16`} src={bandera} alt='' /></a>
+      <a href={`/world_cup_2022/equipo/${abrev}`}>
+        <img className={`${(!clas) ? 'h-16' : 'h-8'} w-28 xs:w-16 xs:h-10 sm:w-28 sm:h-16 shadow-md`} src={bandera} alt={`bandera - ${nombre}`} />
+      </a>
       <h2>{(goles === undefined) ? nombre : `${nombre}-${goles}`}</h2>
     </div>
   )
